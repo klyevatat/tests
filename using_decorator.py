4a813d530e3066458b181fc219fe92fbc8ed4d16
SETaKl <klyevatatt.y@yandex.ru>
2019-09-10 14:54:47 +0300

    1. Создать функцию с множественными декоратороми, которая бы выводила какое-нибудь имя, а декораторы обрамляли бы имя тегами <div>, <span>, <a>

---
+++ using_decorator.py
@@ -0,0 +1,22 @@
+def decorator_tag_div(fn):
+    def wrapped():
+        return '<div>' + fn() + '</div>'
+    return wrapped
+
+def decorator_tag_span(fn):
+    def wrapped():
+        return '<span>' + fn() + '</span>'
+    return wrapped
+
+def decorator_tag_a(fn):
+    def wrapped():
+        return '<a>' + fn() + '</a>'
+    return wrapped
+
+@decorator_tag_a
+@decorator_tag_span
+@decorator_tag_div
+def name():
+    return 'Hello'
+
+print(name())
